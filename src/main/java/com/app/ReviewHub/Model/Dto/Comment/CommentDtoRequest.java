d696c83fb4ce2990ee028e76aedf7044cabe1567
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-02-04 01:40:05 +0100

    Comment DTOs

--- src/main/java/com/app/ReviewHub/Model/Dto/Comment/CommentDtoRequest.java
+++ src/main/java/com/app/ReviewHub/Model/Dto/Comment/CommentDtoRequest.java
@@ -1,12 +1,45 @@
 package com.app.ReviewHub.Model.Dto.Comment;
 
+import com.app.ReviewHub.Model.Entity.Comment;
+import com.app.ReviewHub.Model.Entity.Person;
+import com.app.ReviewHub.Model.Entity.Review;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentDtoRequest {
+    private UUID id;
+
+    @NotBlank(message = "Text Must be Not Blank")
+    private String text;
+
+    @NotNull(message = "User Must be Not Null")
+    private UUID user;
+
+
+    @NotNull(message = "parentComment Must be Not Null")
+    private UUID parentComment;
+
+    @FutureOrPresent(message = "comment must be in the present")
+    private LocalDate date;
+
+
+    @NotNull(message = "review Must be Not Null")
+    private UUID review;
+
+    private Boolean hide;
+
+    @NotNull(message = "blockedBy Must be Not Null")
+    private UUID blockedBy;
 }
--- src/main/java/com/app/ReviewHub/Model/Dto/Comment/CommentDtoResponse.java
+++ src/main/java/com/app/ReviewHub/Model/Dto/Comment/CommentDtoResponse.java
@@ -1,12 +1,35 @@
 package com.app.ReviewHub.Model.Dto.Comment;
 
+import com.app.ReviewHub.Model.Dto.PersonRequest;
+import com.app.ReviewHub.Model.Dto.Review.ReviewDtoReq;
+import com.app.ReviewHub.Model.Entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentDtoResponse {
+    private UUID id;
+
+    private String text;
+
+    private PersonRequest user;
+
+    private Comment parentComment;
+
+    private LocalDate date;
+
+    private ReviewDtoReq review;
+
+    private Boolean hide;
+
+    private PersonRequest blockedBy;
+
+    private List<CommentDtoRequest> childComments;
 }
